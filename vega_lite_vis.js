6965816b27ea2e0e531688c578b2bc1e8f97b44a
ChangLinOng <88361013+HeateX0@users.noreply.github.com>
2023-09-24 18:33:42 +0800

    update

--- vega_lite_vis.js
+++
@@ -1,5 +0,0 @@
-var vg_1 = "asgn2.json";
-vegaEmbed("#bar_chart", vg_1).then(function(result) {
-// Access the Vega view instance
-//"https://raw.githubusercontent.com/FIT3179/Datasets/main/city_daily_cost.csv"
-}).catch(console.error);
